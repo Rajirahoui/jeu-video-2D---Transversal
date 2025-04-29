d75f1649ec44acf33aaeaaa1ba2e34dac0f23dd6
Rajirahoui <rajirahoui1@gmail.com>
2025-04-29 11:34:29 +0200

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+from math import ceil
+import pygame
+pygame.init()
+
+#genere l'image du jeu pour barbie et ken 
+pygame.display.set_caption("spy barbie")
+screen = pygame.display.set_mode(size=(1080, 720))
+
+background = pygame.image.load("assets/background.png")
+
+
+
